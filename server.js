02ff9d19fb19c9aaca7196b3f65294fe3ae13709
janither768 <142415494+janither768@users.noreply.github.com>
2026-06-24 19:11:02 +0530

    EXP 6

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ const renderLanding = (stats = {}) => {
   .terminal-col {
     flex: none;                   /* stretch vertically */
     width: 100%;
-    height: 134px;
+    height: 236px;
     overflow-y: auto;
     scrollbar-width: none;
     -ms-overflow-style: none;
